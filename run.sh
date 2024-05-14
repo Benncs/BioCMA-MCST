61710fc147c0e63948cde88dee35f857dee41dd8
Benncs <benjamin.casale@laposte.net>
2024-05-14 08:56:44 +0200

    update birem

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 
 executable=builddir/release/apps/cli/biocma_mcst_cli_app
-np=5000000
-final_time=30
+np=10000000
+final_time=15
 
-$executable -np $np -d $final_time -r 1  -f  ./cma_data/bench/ 
+$executable -np $np -d $final_time -r 1  -f  ./cma_data/bench_2/ 
 # $executable -np $np -d $final_time -f ./cma_data/sanofi/ 
 
 # $executable -np $np -d $final_time -f ./cma_data/bioreactor_20m3/ 
