3a1dea758fbe733726c3adeba3434e67f65d2dae
bcasale <benjamin.casale@proton.me>
2025-10-29 12:33:59 +0100

    stash tmp changes

--- apps/cli/src/main.cpp
+++ apps/cli/src/main.cpp
@@ -157,7 +157,6 @@ int parse_callback_ok(
   {
     HANDLE_RC(h->register_parameters(
         std::forward<decltype(user_params)>(user_params)));
-    h->set_feed_constant(0.1 / 3600 * 1e-3, 10, 0, 0);
     HANDLE_RC(h->apply(load_serde));
     HANDLE_RC(h->exec());
   }
