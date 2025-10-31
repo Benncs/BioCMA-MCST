59e4689de1628fc6a4f52df3e013d83e6b42c9ff
bcasale <benjamin.casale@proton.me>
2025-10-31 11:56:33 +0100

    feat(common): add wrapper around env variable

---
+++ apps/libs/common/public/common/env_var.hpp
@@ -0,0 +1,52 @@
+#ifndef __BIOMC_COMMON_ENV_VAR_HPP__
+#define __BIOMC_COMMON_ENV_VAR_HPP__
+#include <cstdlib>
+#include <sstream>
+#include <string_view>
+
+namespace Common
+{
+  /**
+   * @brief Wrapper arround get_env to get envariable with fallback to default
+   * @note: UB if T is not trivial type
+   */
+  template <typename T> T read_env_or(std::string_view varname, T vdefault)
+  {
+    const char* env_var = std::getenv(varname.data());
+    T ret = vdefault;
+    if (env_var != nullptr)
+    {
+      std::istringstream stream(env_var);
+      T value;
+      stream >> value;
+      if (stream)
+      {
+        ret = vdefault;
+      }
+    }
+    return ret;
+  }
+
+  /**
+   * @brief Wrapper arround set_env
+   * @note: UB if T is not trivial type
+   */
+  template <typename T> bool set_local_env(std::string_view varname, T val)
+  {
+#ifdef _WIN32
+    // Windows-specific handling (not implemented)
+    std::cerr << "Not implemented for Win32" << std::endl;
+    return false;
+#else
+
+    std::ostringstream oss;
+    oss << val;
+    std::string str_value = oss.str();
+
+    return setenv(varname.data(), str_value.c_str(), 1) == 0;
+#endif
+  }
+
+} // namespace Common
+
+#endif
--- apps/libs/common/src/common.cpp
+++ apps/libs/common/src/common.cpp
@@ -1,10 +1,51 @@
 #include <biocma_cst_config.hpp>
+#include <common/env_var.hpp>
 #include <common/execinfo.hpp>
 #include <string>
-
 std::string ExecInfo::get_version()
 {
   return std::string("v" + std::to_string(_BIOMC_VERSION_MAJOR) + "." +
                      std::to_string(_BIOMC_VERSION_MINOR) + "." +
                      std::to_string(_BIOMC_VERSION_DEV));
 }
+namespace Common
+{
+  template <> bool read_env_or(std::string_view varname, bool vdefault)
+  {
+    const char* env_var = std::getenv(varname.data());
+    if (env_var != nullptr)
+    {
+      if (std::string(env_var) == "true" || std::string(env_var) == "1")
+      {
+        return true;
+      }
+      else if (std::string(env_var) == "false" || std::string(env_var) == "0")
+      {
+        return false;
+      }
+    }
+    return vdefault;
+  }
+
+  template <>
+  bool set_local_env<std::string>(std::string_view varname, std::string val)
+  {
+#ifdef _WIN32
+    std::cerr << "Not implemented for Win32" << std::endl;
+    return false;
+#else
+    return setenv(varname.data(), val.c_str(), 1) == 0;
+#endif
+  }
+
+  template <> bool set_local_env<bool>(std::string_view varname, bool val)
+  {
+#ifdef _WIN32
+    std::cerr << "Not implemented for Win32" << std::endl;
+    return false;
+#else
+    return setenv(varname.data(), val ? "true" : "false", 1) == 0;
+#endif
+  }
+
+} // namespace Common
