76a7d91d363de283382dcd769c875b1c0687d8e2
bcasale <benjamin.casale@proton.me>
2025-11-17 17:31:47 +0100

    feat: add generic read_env with optional type

--- apps/libs/common/public/common/env_var.hpp
+++ apps/libs/common/public/common/env_var.hpp
@@ -1,18 +1,18 @@
 #ifndef __BIOMC_COMMON_ENV_VAR_HPP__
 #define __BIOMC_COMMON_ENV_VAR_HPP__
+#include <cstdio>
 #include <cstdlib>
+#include <optional>
+#include <ostream>
 #include <sstream>
 #include <string_view>
 namespace Common
 {
-  /**
-   * @brief Wrapper arround get_env to get envariable with fallback to default
-   * @note: UB if T is not trivial type
-   */
-  template <typename T> T read_env_or(std::string_view varname, T vdefault)
+
+  template <typename T> std::optional<T> read_env(std::string_view varname)
   {
     const char* env_var = std::getenv(varname.data());
-    T ret = vdefault;
+    std::optional<T> ret = std::nullopt;
     if (env_var != nullptr)
     {
       std::istringstream stream(env_var);
@@ -22,10 +22,21 @@ namespace Common
       {
         std::printf(
             "[Config] Env value load: %s %s\r\n", varname.data(), env_var);
+        std::fflush(stdout);
+
         ret = value;
       }
     }
     return ret;
+  }
+
+  /**
+   * @brief Wrapper arround get_env to get envariable with fallback to default
+   * @note: UB if T is not trivial type
+   */
+  template <typename T> T read_env_or(std::string_view varname, T vdefault)
+  {
+    return read_env<T>(varname).value_or(vdefault);
   }
 
   /**
@@ -39,7 +50,6 @@ namespace Common
     std::cerr << "Not implemented for Win32" << std::endl;
     return false;
 #else
-
     std::ostringstream oss;
     oss << val;
     std::string str_value = oss.str();
